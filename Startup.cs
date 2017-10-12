db6773be004158c8484abc464dac564e93021de3
Aydin Mir Mohammadi <am@bluehands.de>
2017-10-12 17:11:26 +0200

    Some basic inputs to start

---
+++ Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerDemo.Domain.Customer;
+using CustomerDemo.Util.GloblaExceptionHandler;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using WebApiHypermediaExtensionsCore.WebApi.ExtensionMethods;
+
+namespace CustomerDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options =>
+            {
+                options.OutputFormatters.Clear();
+                options.OutputFormatters.Add(new JsonOutputFormatter(
+                    new JsonSerializerSettings
+                    {
+                        NullValueHandling = NullValueHandling.Ignore,
+                        DefaultValueHandling = DefaultValueHandling.Ignore
+                    }, ArrayPool<char>.Shared));
+
+                // Initializes and adds the Hypermedia Extensions
+                options.AddHypermediaExtensions();
+                options.Filters.Add(new GlobalExceptionFilter(null));
+            });
+
+            // Required by Hypermedia Extensions
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+
+            // Domain
+            services.AddSingleton<ICustomerRepository, CustomerRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ Util/ProblemJsonBuilder.cs
@@ -0,0 +1,33 @@
+﻿using WebApiHypermediaExtensionsCore.ErrorHandling;
+
+namespace CustomerDemo.Util
+{
+    public static class ProblemJsonBuilder
+    {
+        public static ProblemJson CreateEntityNotFound()
+        {
+            var problem = new ProblemJson
+            {
+                Title = "Entity not found",
+                Detail = "",
+                ProblemType = "CustomerDemo.EntityNotFound",
+                StatusCode = 404
+            };
+
+            return problem;
+        }
+
+        public static ProblemJson CreateBadParameters()
+        {
+            var problem = new ProblemJson
+            {
+                Title = "Bad Parameters",
+                Detail = "Review parameter schema.",
+                ProblemType = "CustomerDemo.BadParameters",
+                StatusCode = 400
+            };
+
+            return problem;
+        }
+    }
+}
